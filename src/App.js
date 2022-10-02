8016e6417366b783262d8a3da82bdc59c94e0cb9
KateHol <Golotakaterina@gmail.com>
2022-10-02 23:17:24 +0300

    add link

--- src/App.js
+++ src/App.js
@@ -1,2 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
+export default function App(){
+    return(
+        <div>
+            <link href="https://github.com/KateHol/weatherReact" rel="stylesheet">
+>Open-source code</link>, by Kate Holota from She Codes
+        </div>
+    )
 
