0b7fa78c3d982932e1b7fc425aac949369c2e57c
KateHol <Golotakaterina@gmail.com>
2022-10-02 23:20:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 
 export default function App(){
     return(
         <div>
             <link href="https://github.com/KateHol/weatherReact" rel="stylesheet">
->Open-source code</link>, by Kate Holota from She Codes
+Open-source code</link>, by Kate Holota from She Codes.
         </div>
-    )
-
+    )}
