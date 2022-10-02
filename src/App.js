e753c216d932578de7257a9dfb7c693b5561a903
KateHol <Golotakaterina@gmail.com>
2022-10-02 23:05:36 +0300

    add search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,2 @@
-import './App.css';
 
-import Weather from './Weather';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello from react</h1>
-        <Weather />
-      </header>
-    </div>
-  );
-}
-
-export default App;
